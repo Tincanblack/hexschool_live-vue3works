2c277a12aaf8e9077a6f03c42a220ce85ff83575
Ryan <tinblack0920@gmail.com>
2022-02-07 23:55:13 +0800

    week3 done

--- week3/js/list.js
+++ week3/js/list.js
@@ -6,15 +6,15 @@ const app = {
 		return {
 			apiUrl: "https://vue3-course-api.hexschool.io/v2",
 			apiPath: "works",
-			template: {
-				imagesUrl: []
+			targetProduct: {
+				imagesUrl: [],
 			},
 			products: [],
 			isNew: false,
 		};
 	},
 	methods: {
-		renderData() {
+		getProductsList() {
 			axios
 				.get(`${this.apiUrl}/api/${this.apiPath}/admin/products`)
 				.then((res) => {
@@ -32,7 +32,7 @@ const app = {
 				.post(`${this.apiUrl}/api/user/check`)
 				.then((res) => {
 					// 如果成功驗證登入狀態則執行將產品列表render出來
-					this.renderData();
+					this.getProductsList();
 				})
 				.catch((err) => {
 					// 如果無登入情況時瀏覽產品頁面或是驗證失敗會導向至登入頁並重新登入
@@ -42,17 +42,61 @@ const app = {
 		},
 		checkItemDetail(item) {
 			// 當點擊時取得點到的item並將資料複製到temp裡去
-			this.template = { ...item };
+			this.targetProduct = { ...item };
 		},
-		openModal() {
-			productModal.show();
+		openModal(action, item) {
+			if (action === "add") {
+				// 如果是新增將template重置
+				this.targetProduct = {
+					imagesUrl: [],
+				};
+				this.isNew = true;
+				productModal.show();
+			} else if (action === "edit") {
+				// 利用深拷貝將產品拷貝至template
+				this.targetProduct = JSON.parse(JSON.stringify(item));
+				this.isNew = false;
+				productModal.show();
+			} else if (action === "del") {
+				// 利用深拷貝將產品拷貝至template
+				this.targetProduct = JSON.parse(JSON.stringify(item));
+				console.log(this.targetProduct);
+				delProductModal.show();
+			}
 		},
-		// openModal(action, item) {
-		// 	if (action === "new") {
-		// 		this.isNew = true;
-		// 		productModal.show();
-		// 	}
-		// },
+		updateProduct() {
+			// 新增產品 setting
+			let url = `${this.apiUrl}/api/${this.apiPath}/admin/product`;
+			let method = "post";
+			// 更新產品 setting
+			if (!this.isNew) {
+				url = `${this.apiUrl}/api/${this.apiPath}/admin/product/${this.targetProduct.id}`;
+				method = "put";
+			}
+			// AJAX
+			axios[method](url, { data: this.targetProduct })
+				.then((res) => {
+					console.log(res.data.message);
+					this.getProductsList();
+					productModal.hide();
+				})
+				.catch((err) => {
+					console.log(err);
+				});
+		},
+		delProduct() {
+			// 刪除產品
+			axios
+				.delete(`${this.apiUrl}/api/${this.apiPath}/admin/product/${this.targetProduct.id}`)
+				.then((res) => {
+					console.log(res.data.message);
+					this.getProductsList();
+					delProductModal.hide();
+				})
+				.catch((err) => {
+					console.log(err);
+				});
+		},
 	},
 	mounted() {
 		// Vue掛載完成時將token寫入至headers
